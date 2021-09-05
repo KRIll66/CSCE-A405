f2d11a9f962bfbbf55989efaafa4e329c3600c22
KRIll66 <48299310+KRIll66@users.noreply.github.com>
2021-09-04 16:35:10 -0800

    finished main error handling for user input.

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,8 +2,8 @@
 
 
 """
-Marshall Pratt  --
-Lydia Stark     --
+Marshall Pratt  -- mpratt6@alaska.edu
+Lydia Stark     -- lydiarstark@gmail.com
 Chris Hill      -- rhill66@alaska.edu
 
 Assignment 1
@@ -21,9 +21,11 @@ Total  number of nodes expanded
 The maximum number of nodes 'represented' in the search space
 The length of the Solution path
 A Sequence of the Solution path 
+
+@author Chris Hill
 """
 
-import node, list, state, sys 
+import node, nodelists, state, sys 
 
 # slowly developing argument parser. 
 
@@ -32,30 +34,77 @@ def find_repeat(numbers):
     for num in numbers:
         #print(num)
         if num in seen:
-            print('repeating values!')
-            sys.exit()
+            raise ValueError('repeating values!')
         seen.add(num)
 
-def main():
-        arr = input(" Enter digits [0-15], (seperated by commas), in any order as a START state for you 15-puzzle problem.\n") 
-        arr = arr.split(",")
-        if len(arr) < 16:
-                print('too few digits!')
-                sys.exit()
-        if len(arr) > 16:
-                print('too many digits!')
-                sys.exit()
-        find_repeat(arr)
+    
 
-        for index, item in enumerate(arr):
+def main():
+    trigger = True
+    while trigger:
+        start = input("\nEnter digits [0-15], (seperated by commas), in any order as a START state for you 15-puzzle problem.\n") 
+        start = start.split(",")
+        if len(start) < 16:
+                print('\nToo few digits! Please try again.\n')
+                start.clear()
+                continue
+        if len(start) > 16:
+                print('\nToo many digits! Please try again.\n')
+                start.clear()
+                continue
+        try: 
+            find_repeat(start)
+        except(ValueError):
+            print('\nPlease do not repeat values. Please try again.\n')
+            start.clear()
+            continue
+
+        for index, item in enumerate(start):
             #print(index, item)
             temp = int(item)
             if temp > 15 or temp < 0:
-                print('invalid digit, must be 1-15')
-                sys.exit()
-            arr[index] = temp
+                print('\nInvalid digit, must be 1-15. Please try again.\n')
+                start.clear()
+                continue
+            start[index] = temp
             if index % 4 == 3:
-                print("mod")
+                print("\nmod\n")
+                trigger = False
+                break
+    trigger = True
+    while trigger:
+        goal = input("\nNow do using the same format, enter digits [0-15], for a  Goal state.\n") 
+        goal = goal.split(",")
+        if len(goal) < 16:
+                print('\nToo few digits! Please try again.\n')
+                goal.clear()
+                continue
+        if len(goal) > 16:
+                print('\nToo many digits! Please try again.\n')
+                goal.clear()
+                continue
+        try: 
+            find_repeat(goal)
+        except(ValueError):
+            print('\nPlease do not type repeating values. Please try again.\n')
+            goal.clear()
+            continue
+
+        for index, item in enumerate(goal):
+            #print(index, item)
+            temp = int(item)
+            if temp > 15 or temp < 0:
+                print('\nInvalid digit, must be 1-15. Please try again.\n')
+                goal.clear()
+                continue
+            goal[index] = temp
+            if index % 4 == 3:
+                print("\nmod\n")
+                trigger = False
+                break
+    print("\n MADE IT TO END\n")
+    
+    
        
 
         
